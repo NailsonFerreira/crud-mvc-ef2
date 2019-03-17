331834c6b815649c1458e54ef2c2d1954161fd25
Nailson <nailson_ferreira@hotmail.com>
2019-03-17 10:13:29 -0300

    Scaffolding

---
+++ CrudMvcEF/Data/CrudMvcEFContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudMvcEF.Models
+{
+    public class CrudMvcEFContext : DbContext
+    {
+        public CrudMvcEFContext (DbContextOptions<CrudMvcEFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CrudMvcEF.Models.Client> Client { get; set; }
+    }
+}
--- CrudMvcEF/Startup.cs
+++ CrudMvcEF/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using CrudMvcEF.Models;
 
 namespace CrudMvcEF
 {
@@ -33,6 +35,9 @@ namespace CrudMvcEF
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<CrudMvcEFContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("CrudMvcEFContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
