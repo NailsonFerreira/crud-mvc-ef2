1c9ac7927b92c96b1491bb1d42617b4fbac19629
Nailson <nailson_ferreira@hotmail.com>
2019-03-17 10:37:30 -0300

    Primeira tabela

---
+++ CrudMvcEF/Migrations/20190317132819_Initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrudMvcEF.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Cpf = table.Column<string>(nullable: true),
+                    Rg = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Client");
+        }
+    }
+}
--- CrudMvcEF/Startup.cs
+++ CrudMvcEF/Startup.cs
@@ -37,7 +37,8 @@ namespace CrudMvcEF
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<CrudMvcEFContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("CrudMvcEFContext")));
+                    options.UseMySql(Configuration.GetConnectionString("CrudMvcEFContext"), builder => 
+                    builder.MigrationsAssembly("CrudMvcEF")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
